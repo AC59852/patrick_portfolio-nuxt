79081827632044e5ff2147048f09a3aedb55c329
AC59852 <austin.caron1@gmail.com>
2022-03-22 02:05:35 -0400

    adding new slice

---
+++ slices/ResearchSlice/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <section class="project__research">
+    <div class="project__textContent">
+      <prismic-rich-text :field="slice.primary.researchTitle" class="project__title project__title--small" />
+    </div>
+    <prismic-image :field="slice.primary.researchImage" class="project__image project__image--research" />
+    <div class="project__textContent">
+      <prismic-rich-text :field="slice.primary.researchDescription" class="project__text project__text--small"/>
+    </div>
+    <div v-for="(item, i) in slice.items" :key="`slice-item-${i}`" class="project__researchItem">    
+      <prismic-image :field="item.researchImage2" />
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "ResearchSlice",
+  props: {
+    slice: {
+      type: Object,
+      required: true,
+      default() {
+        return {}
+      },
+    },
+  },
+}
+</script>
+
+<style scoped>
+.section {
+  position: relative;
+  background: #f7f7f7;
+  color: #111;
+  padding: 4em;
+  text-align: center;
+}
+a {
+  color: #111;
+}
+.title {
+  margin-bottom: 2em;
+}
+</style>
