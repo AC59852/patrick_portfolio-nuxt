c24f4b6e140dd63d4f46fde0b894c90f0ef707b1
AC59852 <austin.caron1@gmail.com>
2022-03-28 22:32:58 -0400

    fixing SEO

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,9 +13,9 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'og:title', name: 'og:title', content: 'Patrick Bradley UX/UI Designer' },
-      { hid: 'og:image', name: 'og:image', content: "@/assets/images/logo.png" },
-      { hid: 'og:image:alt', name: 'og:image:alt', content: `Patrick Bradley's Logo` },
+      { hid: 'og:title', name: 'og:title', content: 'Patrick Bradley | UX/UI Designer' },
+      { hid: 'og:image', name: 'og:image', content: "@/assets/icons/seo_photo.png" },
+      { hid: 'og:image:alt', name: 'og:image:alt', content: `Hand drawn photo of Patrick Bradley` },
       { hid: 'description', name: 'description', content: 'Patrick Bradley is a London and Toronto, Ontario based UX/UI Designer, focusing on simplifying user interfaces to produce user friendly, engaging, and responsive websites and mobile applications.' },
       { hid: 'og:site_name', name: 'og:site_name', content: 'Patrick Bradley' },
       { name: 'format-detection', content: 'telephone=no' }
