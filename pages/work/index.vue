5bdc1c1ec05233059bc7553569274a6df80ecf05
AC59852 <austin.caron1@gmail.com>
2022-04-20 18:02:31 -0400

    attempting file fix

--- pages/work/index.vue
+++ pages/work/index.vue
@@ -103,11 +103,6 @@ export default {
     return {
         projects: [],
         mobile: false,
-
-        banners: [
-          "https://images.unsplash.com/photo-1645997941789-6816cfc741dd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=695&q=80",
-          "https://images.unsplash.com/photo-1646048032503-556be68ded9e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-          "https://images.unsplash.com/photo-1645974356103-8f8c44581d8f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80"],
       swiperOption: {
         slidesPerView: 'auto',
         navigation: false,
